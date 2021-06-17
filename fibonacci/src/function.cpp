46aa97430fe92503dfede1790f4a3959c415d41c
Pablo Santos <pablosantos.if@gmail.com>
2021-06-17 16:22:53 -0300

    Update function.cpp

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -3,15 +3,20 @@
 std::vector<unsigned int> fib_below_n( unsigned int n )
 {
     // TODO: adicione o seu código aqui.
-	int numAnterior= 1 , numPenultimo=1;
-	fib_below_n.push_back(numPenultimo);
-	fib_below_n.push_back(numAnterior);
+	vector<unsigned int> vetor;
+	int numAnterior = 1, numPenultimo = 1;
+	vetor.push_back(numPenultimo);
+	vetor.push_back(numAnterior);
 
-	int proxTermo = numAnterior+numPenultimo;
+	int proxTermo = numAnterior + numPenultimo;
 
-	while(fib_below_n.back<n){
-		fib_below_n.push_back(proxTermo);
+	while (proxTermo < n) {
+		vetor.push_back(proxTermo);
+		numAnterior = vetor[vetor.size() - 1];
+		numPenultimo = vetor[vetor.size() - 2];
+		proxTermo = numAnterior + numPenultimo;
+	}
     }
     // TODO: Isto é apenas um STUB. Troque o retorno pelo que você julgar correto.
-    return std::vector<unsigned int>{};
+    return vetor;
 }
