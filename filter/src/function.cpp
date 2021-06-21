06c3826826dd9fc6b31d6e1a2e44b67974d728df
Pablo Santos <pablosantos.if@gmail.com>
2021-06-21 17:21:49 -0300

    Update function.cpp

--- filter/src/function.cpp
+++ filter/src/function.cpp
@@ -13,9 +13,37 @@
  * @return a pointer to the new 'logical' end of the array.
  */
 int * filter( int * first, int * last )
+{/*! 
+ * Remove negative and zero values from an array, preserving the
+ * relative order of the original values that will remain in the
+ * resulting array.
+ * We do not alter the origimal memory associated withe the input
+ * array. Rather, we just rearrange the values inside the array
+ * and return a pointer to the new 'logical' end of the array,
+ * after the processes of eliminating the required elements is
+ * finished.
+ *
+ * @param first Pointer to the beginning of the range we want to filter.
+ * @param last Pointer just past the last valid value of the range we want to filter.
+ * @return a pointer to the new 'logical' end of the array.
+ */
+int * filter( int * first, int * last )
 {
-    // TODO: coloque aqui o seu código-solução.
-    
-    // TODO: substitua o retorno de acordo com a solução. Isso é somente um STUB temporário.
+    while(first != last){
+    	if(first<=0){
+    		int aux(first);
+    		while(aux != last-1){
+    			*aux= *(aux+1);
+    			aux++;
+    		}
+    		last--;
+    	}else{
+    		first++;
+    	}
+    }
+
+    return first;
+}
+
     return nullptr;
 }
